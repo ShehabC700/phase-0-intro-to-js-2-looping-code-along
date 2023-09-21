9c8fdbb7a1ca315d42025ac6084fd92918b9cbbd
Shehab Chowdhury <shehabc58@gmail.com>
2023-09-21 01:15:44 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = [ `Guadalupe!`, `Ollie!`, `Aki` ]
+
+function writeCards(names) {
+    let thankYouMes = []
+
+    for (let i = 0; i < names.length; i++) {
+    let message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    thankYouMes.push(message);
+    console.log(message)
+} 
+    return thankYouMes;
+}
+
+function countDown() {
+    let number = 10
+
+    while (number > -1) {
+        console.log(number--)
+    }
+}
